94eb16b6eafd4cd682c970986d99be4374ee3d49
hellennakabuye <55150746+hellennakabuye@users.noreply.github.com>
2023-09-25 16:11:33 +0300

    Update fasta.sh

--- fasta.sh
+++ fasta.sh
@@ -4,8 +4,8 @@ minimap2 -x ava-ont -t32 /webapp/media/ecoli.fasta /webapp/media/ecoli.fasta -K5
 miniasm -2S6 -f/webapp/media/ecoli.fasta /webapp/media/ecoli.paf.gz > /webapp/media/ecoli.gfa
 grep '^S' /webapp/media/ecoli.gfa | awk '{print">"$2"\n"$3}' > /webapp/media/ecoli_contigs.fasta
 abricate  /webapp/media/ecoli_contigs.fasta > /webapp/media/myfiles
-# grep ">" ${file}_contigs.fasta | wc -l
-# assembly-stats ./${file}_contigs.fasta
+grep ">" ${file}_contigs.fasta | wc -l
+assembly-stats ./${file}_contigs.fasta
 
 
 
